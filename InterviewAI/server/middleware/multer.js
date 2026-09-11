2d30161735a9953953c3ea82368d35266c707b1c
Jaswant singh <jaswanttyagi66@gmail.com>
2026-09-11 12:10:53 +0530

    Refactor multer middleware to create upload directory

--- InterviewAI/server/middleware/multer.js
+++ InterviewAI/server/middleware/multer.js
@@ -1,17 +1,29 @@
-const multer = require("multer")
-const storage = multer.diskStorage({
-    destination : function(req , file , cb){
-        cb(null , "public")
-    },
-    filename: function(req , file , cb){
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null , filename);
-    }
-})
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
- const upload = multer({
-    storage ,
-    limits : {fileSize : 5 * 1024 * 1024}, // 5 MB
+const uploadDir = path.join(__dirname, "../public");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+
+    filename: function (req, file, cb) {
+        const filename = Date.now() + "-" + file.originalname;
+        cb(null, filename);
+    }
 });
 
-module.exports = {upload , storage};
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
+
+module.exports = { upload, storage };
