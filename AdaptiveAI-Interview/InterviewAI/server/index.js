6faa7119572b80b3c8e8385c0f07153c47e17f5d
Jaswant singh <jaswanttyagi66@gmail.com>
2026-09-16 17:34:00 +0530

    Update index.js

--- AdaptiveAI-Interview/InterviewAI/server/index.js
+++ AdaptiveAI-Interview/InterviewAI/server/index.js
@@ -15,7 +15,7 @@ const app = express();
 // CORS
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://client-lrqr9splg-jaswanttyagi66-3224s-projects.vercel.app"
+    "https://client-beta-azure-95.vercel.app"
 ];
 
 app.use(cors({
